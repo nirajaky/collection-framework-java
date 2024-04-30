4b252b0f7bb1a3b519a855fb3f6d45e471df2b41
Niraj Mehta <niraj86995@gmail.com>
2024-04-30 14:24:41 +0530

    Generic List

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.example.genericlist.OurGenericList;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting...");
+
+        OurGenericList<Integer> list = new OurGenericList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        Iterator<Integer> integerIterator = list.iterator();
+        while (integerIterator.hasNext()) {
+            System.out.println(integerIterator.next());
+        }
+        // OR (Both are same)
+        for (int item : list) {
+            System.out.println(item);
+        }
+
+        System.out.println("Ending...");
+    }
+}
